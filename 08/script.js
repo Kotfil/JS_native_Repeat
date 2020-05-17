5f751fe3ee86e8212425873ebc802e09face7a23
FIlipp <filippkotenko@outlook.com>
2020-05-17 16:57:42 +0300

    newNativeRepeat

---
+++ 08/script.js
@@ -0,0 +1,12 @@
+let arr1 = ['Semen','Vitalik','Sasha','Alena','Igor'];
+
+let arr2 = arr1.map((e) => {
+    if (arr3(el)) {
+        return 1;
+    }else{
+        return 0;
+    }
+});
+let arr2 = arr3;
+
+console.log (arr3);
