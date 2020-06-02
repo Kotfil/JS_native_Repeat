f875477e0bf0249dbe9768830f502dbe54c30361
FIlipp <filippkotenko@outlook.com>
2020-06-02 23:55:06 +0300

    setInterval test

--- 08/script.js
+++ 08/script.js
@@ -1,12 +1,34 @@
-let arr1 = ['Semen','Vitalik','Sasha','Alena','Igor'];
+// let arr1 = ['Semen','Vitalik','Sasha','Alena','Igor'];
+//
+// let arr2 = arr1.map((e) => {
+//     if (arr3(el)) {
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+// });
+// let arr2 = arr3;
+//
+// console.log (arr3);
 
-let arr2 = arr1.map((e) => {
-    if (arr3(el)) {
-        return 1;
-    }else{
-        return 0;
+
+cycle = () => {
+    let a = [1, 2, 3, 4, 5, 6, 7];
+    for (i = 0; i < a.length; i++) {
+        console.log(a[i])
     }
-});
-let arr2 = arr3;
+}
+alerter = () => {
+    setTimeout("alert('Привет')", 1000);
+        clearTimeout()
+}
 
-console.log (arr3);
+
+q = document.getElementById(1);
+w = document.getElementById(2);
+
+
+
+
+
+
