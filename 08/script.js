8c94933fe2479d2da452985f2626c2b9f420c5a6
FIlipp <filippkotenko@outlook.com>
2020-06-03 00:34:14 +0300

    setInterval test 2

--- 08/script.js
+++ 08/script.js
@@ -12,22 +12,22 @@
 // console.log (arr3);
 
 
-cycle = () => {
-    let a = [1, 2, 3, 4, 5, 6, 7];
-    for (i = 0; i < a.length; i++) {
-        console.log(a[i])
-    }
-}
-alerter = () => {
-    setTimeout("alert('Привет')", 1000);
-        clearTimeout()
-}
+// cycle = () => {
+//     let a = [1, 2, 3, 4, 5, 6, 7];
+//     for (i = 0; i < a.length; i++) {
+//         console.log(a[i])
+//     }
+// }
+// alerter = () => {
+//     setTimeout("alert('Привет')", 1000);
+//         clearTimeout()
+// }
+//
+//
+// q = document.getElementById(1);
+// w = document.getElementById(2);
 
-
-q = document.getElementById(1);
-w = document.getElementById(2);
-
-
+console.log('Hello')
 
 
 
