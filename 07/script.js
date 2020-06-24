7604b86a0d502ff5896cd47cff51f0e7f75b0306
FIlipp <filippkotenko@outlook.com>
2020-06-24 15:10:07 +0300

    var let const

--- 07/script.js
+++ 07/script.js
@@ -1,18 +1,18 @@
-let a = {
-    name: 'Fillipko',
-    protocool: "https",
-    maxStudentsCount: 10,
-    isOnline: true,
-    students: ['ivan', 'andrey', 'farid'],
-    classroom: {
-        teather: {
-            name: 'wew',
-            age:18
-        }
-    }
-}
-
-let b = {...a};
+// let a = {
+//     name: 'Fillipko',
+//     protocool: "https",
+//     maxStudentsCount: 10,
+//     isOnline: true,
+//     students: ['ivan', 'andrey', 'farid'],
+//     classroom: {
+//         teather: {
+//             name: 'wew',
+//             age:18
+//         }
+//     }
+// }
+//
+// let b = {...a};
 
 // a.name = 'Blabla';
 // a.isOnline = true;
@@ -21,8 +21,25 @@ let b = {...a};
 
 
 
-b.name = '21333'
-// console.log (a);
-b.students.shift('Vadim')
-b.students.unshift('Vlad')
-console.log (b);
+// b.name = '21333'
+// // console.log (a);
+// b.students.shift('Vadim')
+// b.students.unshift('Vlad')
+// console.log (b);
+// const p = prompt('text')
+// console.log(typeof p)
+
+// const a = ['Fil'];
+//
+// a.push('Viktorya');
+//
+// alert(a)
+//
+//
+// const person = {
+//     name: 'Fil',
+//     age: '22'
+// }
+//
+// person.name = 'Bob'
+// console.log(person)
